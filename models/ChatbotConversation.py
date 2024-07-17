a933474480b898b2fbefbdbfce170360a43badc6
fauzanmhr <fauzan432003@gmail.com>
2024-07-17 18:00:53 +0700

    fix chatbot history database model

--- models/ChatbotConversation.py
+++ models/ChatbotConversation.py
@@ -1,7 +1,5 @@
 from datetime import datetime, timezone
-
 from sqlalchemy import Column, Integer, DateTime, ForeignKey, CHAR, Text
-
 from .Base import Base
 
 
@@ -9,6 +7,6 @@ class ChatbotConversation(Base):
     __tablename__ = 'chatbot_conversations'
     conversation_id = Column(Integer, primary_key=True, index=True, nullable=False, autoincrement=True)
     user_id = Column(CHAR(36), ForeignKey("users.user_id"), index=True)
-    timestamp = Column(DateTime, default=datetime.now(timezone.utc))
+    timestamp = Column(DateTime, default=lambda: datetime.now(timezone.utc))
     message = Column(Text, nullable=False)
     response = Column(Text, nullable=False)
