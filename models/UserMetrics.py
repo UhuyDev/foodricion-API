f85bcfc60699abbcc308b293794acfaade4fd230
fauzanmhr <fauzan432003@gmail.com>
2024-07-23 10:33:34 +0700

    change name from UserDetail to UserMetrics

--- models/UserMetrics.py
+++ models/UserMetrics.py
@@ -5,7 +5,7 @@ from .Base import Base
 
 class UserMetrics(Base):
     __tablename__ = 'users_metrics'
-    user_detail_id = Column(Integer, primary_key=True, index=True, nullable=False, autoincrement=True)
+    user_metrics_id = Column(Integer, primary_key=True, index=True, nullable=False, autoincrement=True)
     user_id = Column(CHAR(36), ForeignKey("users.user_id"), index=True)
     age = Column(Integer)
     height = Column(Integer)
